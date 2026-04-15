4b9c0f60d7c2d0d263ee3212c1d50090daaedc41
AMRR3Dev <alejandromanramirez20@gmail.com>
2026-04-15 15:23:26 -0600

    Se suben pruebas de java con terminal

---
+++ holamundo_l.java
@@ -0,0 +1,13 @@
+public class holamundo_l{
+
+ public static void main (String [] args){
+
+
+          //se hace esta prueba desde un vi holamundo_l.java
+	 System.out.println("Hola mundo");
+	 System.out.println("esta prueba se hizo desde la terminal de linux con vi");
+ }
+ 
+}
+
+
